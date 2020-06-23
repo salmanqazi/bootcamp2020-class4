be5bd2791b908555e3f555c45414a3b237f6841c
salmanqazi <salmanqazi@users.noreply.github.com>
2020-06-23 21:55:39 +0500

    update App.js with morning and night code

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 
 export default function App() {
   let [count, setCount] = useState(0);
-  return (
-    <div className="App">
+  let [isMorning, setMorning] = useState(true);
+  return (    
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+      <h1>Day time = {isMorning ? 'Morning' : 'Night'}</h1>
       <Message counter={count} />
       <br />
-      <button onClick={() => setCount(count + 1)}>
+      <button className="btn" onClick={() => setCount(count + 1)}>
         Update Counter
+      </button>
+      <button className="btn" onClick={() => setMorning(!isMorning)}>
+        Update Day
       </button>
     </div>
   );
